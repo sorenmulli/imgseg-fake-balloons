11aff9f6c6c64ee1f02ede91efaec2ebec09af71
sorenmulli <swholm@protonmail.com>
2022-11-21 15:32:37 +0100

    Start task 1

---
+++ scene.js
@@ -0,0 +1,53 @@
+const lightPosVal = vec4(0.0, 0.0, 1.0, 0.0)
+const LeRadiance = 1.0;
+const LeVal = vec3(LeRadiance, 0.0, 0.0);
+const kaVal = 0.1;
+const kdVal = 0.8;
+const ksVal = 0.3;
+const sVal = 100;
+
+const alpha = 45.0;
+const n = 1.0;
+const f = 100.0;
+
+const bgcolor = vec4(0.0, 0.0, 0.0, 1.0)
+
+function setup(canvas) {
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor(bgcolor[0], bgcolor[1], bgcolor[2], bgcolor[3]);
+    gl.enable(gl.DEPTH_TEST);
+    gl.enable(gl.CULL_FACE);
+    gl.program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(gl.program);
+    gl.vBuffer = null;
+    gl.nBuffer = null;
+
+    return gl;
+}
+
+function light(gl) {
+    gl.uniform4fv(gl.getUniformLocation(gl.program, "lightPos"), flatten(lightPosVal));
+    gl.uniform3fv(gl.getUniformLocation(gl.program, "Le"), flatten(LeVal));
+    gl.uniform1f(gl.getUniformLocation(gl.program, "ka"), kaVal);
+    gl.uniform1f(gl.getUniformLocation(gl.program, "kd"), kdVal);
+    gl.uniform1f(gl.getUniformLocation(gl.program, "ks"), ksVal);
+    gl.uniform1f(gl.getUniformLocation(gl.program, "s"), sVal);
+}
+
+function view(gl, canvas) {
+    let A = canvas.width / canvas.height;
+
+    let projectionTransform = perspective(alpha, A, n, f);
+    gl.uniformMatrix4fv(gl.getUniformLocation(gl.program, "P"), false, flatten(projectionTransform));
+
+    eye = vec3(0.0, 0.0, 5.0);
+    up = vec3(0.0, 1.0, 0.0);
+    at = vec3(0.0, 0.0, 0.0);
+    let viewMat = lookAt(eye, at, up);
+    gl.uniformMatrix4fv(gl.getUniformLocation(gl.program, "V"), false, flatten(viewMat));
+    let N = normalMatrix(viewMat, true);
+    gl.uniformMatrix3fv(gl.getUniformLocation(gl.program, "N"), false, flatten(N));
+}
---
+++ t1.js
@@ -0,0 +1,48 @@
+var gl;
+var LeVarying = vec3(1.0, 0.0, 0.0);
+var orientVal = 0.5;
+
+window.onload = function init()
+{
+    let canvas = document.getElementById("canvas");
+    gl = setup(canvas)
+
+    initSphere(gl, numTimesToSubdivide);
+    light(gl);
+    view(gl, canvas);
+
+    let parent = document.querySelector('#color-parent');
+    let picker = new Picker({parent: parent, color: 'red', alpha: false, editor: false});
+    picker.onChange = function(color) {
+        parent.style.background = color.rgbaString;
+        LeVarying = vec3(color.rgba[0]/255, color.rgba[1]/255, color.rgba[2]/255);
+    };
+
+    document.getElementById("orient-slider").onchange = function() {
+        orientVal = event.srcElement.value;
+    };
+
+
+    render();
+}
+
+function getModel(orient) {
+    let sx = orient * 2.0;
+    let sy = (1 - orient) * 2.0;
+    let norm = 2 ** 0.5 / (sx**2 + sy**2)**0.5;
+    return scalem(sx*norm, sy*norm, 1.0);
+}
+
+
+function render()
+{
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    gl.uniform3fv(gl.getUniformLocation(gl.program, "Le"), flatten(LeVarying));
+    for(let i=0; i<gl.pointsArray.length; i+=3)
+        gl.drawArrays(gl.TRIANGLES, i, 3);
+
+    gl.uniformMatrix4fv(gl.getUniformLocation(gl.program, "M"), false, flatten(getModel(orientVal)));
+
+    requestAnimationFrame(render)
+}
